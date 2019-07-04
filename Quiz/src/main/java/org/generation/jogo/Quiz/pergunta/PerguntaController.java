9ffcf35ddb2ead6c176216212b41b8f7cf03a5dc
GabrielANM <biel-a@hotmail.com>
2019-07-04 20:12:21 -0300

    adicionando os metodos do crud

--- Quiz/src/main/java/org/generation/jogo/Quiz/pergunta/PerguntaController.java
+++ Quiz/src/main/java/org/generation/jogo/Quiz/pergunta/PerguntaController.java
@@ -1,8 +1,8 @@
 package org.generation.jogo.Quiz.pergunta;
 
+import org.generation.jogo.Quiz.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,9 +12,33 @@ public class PerguntaController {
     @Autowired
     private PerguntaRepository perguntaRepository;
 
+    // READ
     @GetMapping("/perguntas")
     public List<Pergunta> findAll() {
         return perguntaRepository.findAll();
     }
+
+    // CREATE
+
+    @PostMapping("/perguntas")
+    public Pergunta save (@RequestBody Pergunta pergunta) {
+        return perguntaRepository.save(pergunta);
+    }
+
+    @PutMapping("/perguntas/{id_pergunta}")
+    public Pergunta update (@PathVariable Long id_pergunta, @RequestBody Pergunta pergunta) throws ResourceNotFoundException {
+        return perguntaRepository.findById(id_pergunta).map(p -> {
+            p.setNumero(pergunta.getNumero());
+            p.setDescricao(pergunta.getDescricao());
+            p.setValor_pontuacao(pergunta.getValor_pontuacao());
+            return perguntaRepository.save(p);
+        }) .orElseThrow(() -> new ResourceNotFoundException("Não existe pergunta cadastrada com o id_pergunta" + id_pergunta));
+    }
+
+    @DeleteMapping("/perguntas/{id_pergunta}")
+    public void delete (@PathVariable Long id_pergunta) {
+        perguntaRepository.deleteById(id_pergunta);
+    }
+
 
 }
